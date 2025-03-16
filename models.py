903a6d37e94b5ce994443a557db8b1afecdeabc1
mikoajp <mikoajmicht@gmail.com>
2025-03-16 16:46:37 +0100

    feat: integer added

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 import uuid
-from sqlalchemy import Column, String, Float, ForeignKey
+from sqlalchemy import Column, String, Float, ForeignKey, Integer
 from sqlalchemy.dialects.postgresql import UUID
 from database import Base
 
